a9365af243604b3d501ee9ccc9276cb3b60ce67c
Anna <annya99124@gmail.com>
2018-01-24 15:47:16 +0300

    1

---
+++ src/main/java/IpAddressValidator.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class IpAddressValidator {
+
+
+    public void inputIPRange(){
+
+        System.out.println("Введите диапазон ip:");
+        Scanner scan = new Scanner(System.in);
+        String ipAddressInput = scan.nextLine();
+        String ipAddressInput2 = scan.nextLine();
+        if (!validate(ipAddressInput)||!validate(ipAddressInput2)){
+            System.out.println("Недопустимое значение IP");
+        }
+
+    }
+
+
+    private boolean validate(String ipAddressInput) {
+        Matcher matcher;
+        Pattern pattern;
+        final String IP_ADDRESS_PATTERN
+                = "^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\."
+                + "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\."
+                + "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\."
+                + "([01]?\\d\\d?|2[0-4]\\d|25[0-5])$";
+
+            pattern = Pattern.compile(IP_ADDRESS_PATTERN);
+
+            return pattern.matcher(ipAddressInput).matches();
+        }
+
+
+
+
+
+
+
+}
+
