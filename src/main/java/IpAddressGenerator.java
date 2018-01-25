f7ed99b8e8ccf65ca82b53b75253f1a6c532dbab
Anna <annya99124@gmail.com>
2018-01-26 00:30:50 +0300

    work version 2.0

--- src/main/java/IpAddressGenerator.java
+++ src/main/java/IpAddressGenerator.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class IpAddressGenerator {
 
-    public void generate(String ipAdress1, String ipAdress2){
+    public void generate(String ipAdress1, String ipAdress2) {
         ArrayList<Integer> mass1Int = new ArrayList<Integer>();
         ArrayList<Integer> mass2Int = new ArrayList<Integer>();
         String[] ipMass1;
@@ -13,27 +13,59 @@ public class IpAddressGenerator {
         for (int i = 0; i <= 3; i++) {
 
 
-             mass1Int.add(Integer.parseInt(ipMass1[i]));//создаем массив чисел из строки
+            mass1Int.add(Integer.parseInt(ipMass1[i]));//создаем массив чисел из строки
             mass2Int.add(Integer.parseInt(ipMass2[i]));
         }
+        int i = mass1Int.get(0);
+        int j = mass1Int.get(1);
+        int k = mass1Int.get(2);
+        int n = mass1Int.get(3);
 
 
-        for (int i =  mass1Int.get(0); i <= mass2Int.get(0); i++) {
-            for (int j = mass1Int.get(1); j <= mass2Int.get(1); j++) {
-                for (int k = mass1Int.get(2); k <= mass2Int.get(2); k++) {
-                    for (int n = mass1Int.get(3); n <= mass2Int.get(3); n++) {
-                        System.out.println(i + "." + j + "." + k + "." +n);
+        while (i < mass2Int.get(0)) {
+            while (j <= 255) {
+                while (k <= 255) {
+                    while (n <= 255) {
+                        System.out.println(i + "." + j + "." + k + "." + n);
+                        n++;
+
                     }
+                    n = 0;
+                    k++;
                 }
+                k = 0;
+                j++;
             }
+            j = 0;
+            i++;
         }
-
-
+        while (j < mass2Int.get(1)) {
+            while (k <= 255) {
+                while (n <= 255) {
+                    System.out.println(i + "." + j + "." + k + "." + n);
+                    n++;
+                }
+                n = 0;
+                k++;
             }
-        
-
-
+            k = 0;
+            j++;
         }
+        while (k < mass2Int.get(2)) {
+            while (n <= 255) {
+                System.out.println(i + "." + j + "." + k + "." + n);
+                n++;
+            }
+            n = 0;
+            k++;
+        }
+        while (n < mass2Int.get(3)) {
+            System.out.println(i + "." + j + "." + k + "." + n);
+            n++;
+        }
+        System.out.println(i + "." + j + "." + k + "." + n);
+    }
+}
 
 
 
